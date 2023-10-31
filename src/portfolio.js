b8d7a7c890289417e7c1a67a88099355ddfc67ff
Rando R <r.rommot@gmail.com>
2023-11-01 00:27:44 +0200

    Update on EE

--- src/portfolio.js
+++ src/portfolio.js
@@ -193,7 +193,7 @@ const workExperiences = {
       company: "Eesti Energia",
       companylogo: require("./assets/images/eestiEnergiaLogo.png"),
       date: "June 2023 – Present",
-      desc: "Energy Trading Risk Management and Physical Electricity Trading",
+      desc: "Energy Trading Risk Management, Physical Electricity Trading, Flexible Services, e-Mobility",
       descBullets: [
         "SLA/SLI/SLO creation, management and monitoring",
         "Incident management",
